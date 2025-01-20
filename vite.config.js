a439a50cc1e9e2e503a2e9a4a125417ad35e5746
Calvin <calvinjose.ritaantony@in.bosch.com>
2025-01-20 20:19:23 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-})
+  base: './', // Ensures relative paths for assets
+  build: {
+    outDir: 'dist', // Ensure output directory matches the `serve` script
+  },
+});
